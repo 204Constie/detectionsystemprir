e275e675c3530d2a2b43625c425e25b0bd1a94db
204Constie <kat.bana204@gmail.com>
2017-12-06 02:48:01 +0100

    init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+P
+ src/ImageConverterInterface.java,8/f/8feefbe746a8eba988c4cb9858254d7cad468687
+P
+ src/ResultConsumerInterface.java,0/a/0a7767e45cb90e4dd7c4912ff04f6f09377d2c89
+K
+src/PMO_ResultListener.java,0/d/0de52fada90637a2172b4cceab57e49e16714857
+W
+'src/MotionDetectionSystemInterface.java,8/7/879c17bd5e3d74fe465caa1c3caa7972965e0db1
+N
+src/MotionDetectionSystem.java,9/f/9fd36be789c3800dcb9c6bf3993b3866d7c1beec
+B
+src/PMO_Start.java,9/c/9c5e834ea7c56c40438d359db31add2cadb63a2e
+B
+src/PMO_TestA.java,c/c/ccf6bc83695e4104205559b706b2f7c561c7a006
+L
+src/PMO_CountersFactory.java,1/1/1163f27a34558b7f3c45b144fe8c1bb70464b8ca
---
+++ src/MotionDetectionSystem.java
@@ -0,0 +1,134 @@
+import org.omg.CORBA.FREE_MEM;
+import sun.jvm.hotspot.jdi.BooleanTypeImpl;
+
+import java.awt.geom.Point2D;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by constie on 01.12.2017.
+ */
+public class MotionDetectionSystem implements MotionDetectionSystemInterface {
+    private TreeMap<Integer, Boolean> frameStatuses = new TreeMap<>();
+    private TreeMap<Integer, int[][]> frameImages = new TreeMap<>();
+//    private Map<Integer, Point2D.Double> results = Collections.synchronizedMap(new TreeMap<>());
+    private ImageConverterInterface imgConvrt;
+    private ResultConsumerInterface rcinter;
+    private Point2D.Double imgResults;
+    private int threadsNo = 0;
+    private int iter = 0;
+    private List<Integer> pairsList = Collections.synchronizedList(new ArrayList<>());
+    private Map<Integer, Point2D.Double> results = Collections.synchronizedMap(new TreeMap<>());
+
+    @Override
+    public void setThreads(int threads) {
+        threadsNo = threads;
+//        threads to ilosc watkow ktora ma wykonywac prace
+//        w trakcie pracy programu moze sie zmieniac:
+//        -  zwiekszenie liczby watkow - jak najszybsze zagospodarownaie
+//        -  zmienjszenie liczby watkow - stopniowe ich wygaszanie
+
+    }
+
+    @Override
+    public void setImageConverter(ImageConverterInterface ici) {
+        imgConvrt = ici;
+
+//        jdenokrotne wykonanie przed pierwszym dostarczeniem obrazu
+//        przekazanie referencji do obiektu odpowiedzialnego za przetwarzanie obrazu
+
+        for(int i=0; i<threadsNo; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (pairsList) {
+                        try {
+                            pairsList.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    while (!pairsList.isEmpty()) {
+
+                        int frameNo;
+                        synchronized (pairsList){
+                            frameNo = pairsList.get(0);
+                           if(frameImages.get(frameNo + 1) == null){
+                               break;
+                           }
+                            pairsList.remove(0);
+                        }
+                        Point2D.Double result = imgConvrt.convert(frameNo, frameImages.get(frameNo), frameImages.get(frameNo + 1));
+
+                        results.put(frameNo, result);
+                        sendResults();
+
+                    }
+
+                }
+            }).start();
+
+        }
+
+    }
+
+    @Override
+    public void setResultListener(ResultConsumerInterface rci) {
+        rcinter = rci;
+
+//        jdenokrotne wykonanie przed pierwszym dostarczeniem obrazu
+//        przekazywanie wynikow do listenera
+
+    }
+
+     synchronized public void sendResults(){
+        PMO_SystemOutRedirect.println("results.get(iter): " + results.get(iter));
+        while(results.get(iter) != null){
+            rcinter.accept(iter, results.get(iter));
+            results.remove(iter);
+            iter++;
+
+        }
+    }
+
+    @Override
+    public void addImage(int frameNumber, int[][] image) {
+
+//        przekazywanie obrazu do przetworzenia
+//        wg frameNumber (od 0 wlacznie)
+//        obrazy moga byc przekazywanie w dowolnej kolejnosci
+//        wszystkie obrazy maja ten sam rozmiar
+
+        synchronized (frameImages) {
+            frameImages.put(frameNumber, image);
+        }
+        synchronized (frameStatuses){
+            frameStatuses.put(frameNumber, Boolean.FALSE);
+        }
+
+        synchronized (pairsList) {
+            pairsList.add(checkFirstPair(frameStatuses));
+            pairsList.notify();
+        }
+
+    }
+
+     private int checkFirstPair(TreeMap<Integer, Boolean> framesArray){
+
+        int lowestFrame = framesArray.firstKey();
+        int secondLowestFrame = framesArray.higherKey(lowestFrame);
+        while(secondLowestFrame - lowestFrame != 1 || framesArray.get(lowestFrame)){
+            lowestFrame = secondLowestFrame;
+            if(framesArray.lastKey() == lowestFrame) {
+                framesArray.put(lowestFrame, Boolean.TRUE);
+                return lowestFrame;
+            }
+            secondLowestFrame = framesArray.higherKey(lowestFrame);
+        }
+        framesArray.put(lowestFrame, Boolean.TRUE);
+        return lowestFrame;
+
+    }
+}
